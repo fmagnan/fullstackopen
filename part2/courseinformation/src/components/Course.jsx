86f0f94f70d7f9c7fd05f110664840de27d6b37e
Franck Magnan <fmagnan@gmail.com>
2026-09-15 09:05:09 +0200

    feat: separating module for course info step 10

--- part2/courseinformation/src/components/Course.jsx
+++ part2/courseinformation/src/components/Course.jsx
@@ -1,6 +1,41 @@
-import Content from "./Content";
-import Header from "./Header";
-import Total from "./Total";
+const Header = ({ name }) => {
+  return (
+    <>
+      <h2>{name}</h2>
+    </>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <>
+      <p>
+        {part.name} {part.exercises}
+      </p>
+    </>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((n, { exercises }) => n + exercises, 0);
+
+  return (
+    <>
+      <strong> total of {total} exercises </strong>
+    </>
+  );
+};
+
 
 const Course = ({ course }) => {
   return (
