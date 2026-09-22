6a38764de598abfe263afe7d7b114df7b996a4db
Franck Magnan <fmagnan@gmail.com>
2026-09-22 09:13:20 +0200

    feat: adding mongodb features

---
+++ part3/phonebook_backend/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://fullstack:${password}@phonebook.pcmhfea.mongodb.net/phonebook?appName=phoneBook`;
+mongoose.set("strictQuery", false);
+mongoose.connect(url, { family: 4 });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (name !== undefined && number !== undefined) {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+
+  person.save().then((result) => {
+    console.log("person saved!");
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+}
